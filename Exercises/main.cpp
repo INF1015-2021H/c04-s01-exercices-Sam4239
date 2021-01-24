cae184a094a710fd4b2fa339738c80ac71a229cb
Sam4239 <71023108+Sam4239@users.noreply.github.com>
2021-01-24 10:04:53 -0500

    Presque termine

--- Exercises/main.cpp
+++ Exercises/main.cpp
@@ -21,26 +21,64 @@ using namespace std::literals;
 
 
 // TODO: Déclarer une constante pour le facteur des multiples à calculer.
+const int factor = 3;
 // TODO: Déclarer une constante pour le taux de taxation.
+const double taxRate = 0.15;
 
 
 int main(int argc, char* argv[]) {
 	// Exo 2 : Arguments du programme
 	// TODO: Traiter les arguments de ligne de commande.
+	//for (int i = 0; i < argc; i++)
+		//cout << argv[i] << "\n";
+	Arguments args = parseArgs(argc, argv);
+	cout << args.number << ", " << args.filename << "\n\n";
+
 
 	// Exo 1 : Bonjour, le monde
 	// TODO: Faire un hello world.
+	cout << "Hello World!" << "\n\n";
 
 	// Exo 3 : Multiples de 3
 	// TODO: Afficher les multiples de 3 plus petits que `number` (passé en paramètre).
+	for (int i : iter::range(factor, args.number, factor))
+		cout << i << " ";
+	cout << "\n\n";
 
 	// Exo 4 : Facture
 	// TODO: Ouvrir le fichier et vérifier l'ouverture.
+	ifstream file(args.filename);
+	file.exceptions(ios::failbit);
+	/*ifstream file(args.filename);
+	if (file.fail()) {
+		cout << "Error in opening" << args.filename << "\n";
+		return EXIT_FAILURE;*/
 	// TODO: Lire le nom du client.
+	string clientName;
+	getline(file, clientName);
 	// TODO: Pour chaque ligne suivante:
+	//ws = whitespaceskip
+	double subTotal = 0.0;
+	while (not ws(file).eof()) {
 		// TODO: Lire le nom de l'item, sa quantité et son prix unitaire.
+		struct Item {
+			string name;
+			int quantity;
+			double cost;
+		};
+		Item item;
+		file >> item.name >> item.quantity >> item.cost;
 		// TODO: Ajouter au sous-total.
+		subTotal += item.quantity * item.cost;
+	};
 	// TODO: Calculer les taxes et le total avec taxes.
+	double taxes = subTotal * taxRate;
+	double total = subTotal + taxes;
 	// TODO: Afficher le nom du client, son sous-total, taxes et total (alignés sur 10 colonnes, deux décimales).
+	cout << clientName << "\n"
+	//<< "SUB TOTAL " << setw(10) << fixed << setprecision(2) << subTotal << " $" << "\n"
+	printf("TAXES%10.2f $" "\n", taxes);
+	//<< "TAXES     " << taxes << "\n"
+	//<< "TOTAL     " << total << "\n";
 }
 
