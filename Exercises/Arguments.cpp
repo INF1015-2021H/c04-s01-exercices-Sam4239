fda9b2c38183d6e1ae12ee9bcee8d04d37d89118
Sam4239 <71023108+Sam4239@users.noreply.github.com>
2021-04-08 18:40:33 -0400

    termine termine

--- Exercises/Arguments.cpp
+++ Exercises/Arguments.cpp
@@ -1,3 +1,52 @@
+/////
+///// Traite les arguments de ligne de commande à l'aide de TCLAP.
+/////
+//
+//
+//#include <cstddef>
+//#include <cstdint>
+//
+//#include <string>
+//
+//#include <tclap/CmdLine.h>
+//
+//#include "Arguments.hpp"
+//
+//using namespace std;
+//using namespace std::literals;
+//
+//
+//Arguments parseArgs(int argc, char* argv[]) {
+//	TCLAP::CmdLine parser("What is it, citizen?");
+//
+//	// Exo 2 : Arguments du programme
+//	// TODO: Créer l'argument de nombre (--number)
+//	TCLAP::ValueArg<int> numberArg(
+//		"",
+//		"number",
+//		"The number used to calculate factorials and smaller multiples",
+//		true,
+//		0,
+//		"int"
+//	);
+//	parser.add(numberArg);
+//	
+//	// TODO: Créer l'argument de fichier (-f et --filename)
+//	TCLAP::ValueArg<string> filenameArg(
+//		"f",
+//		"filename",
+//		"The file to read",
+//		true,
+//		"",
+//		"string"
+//	);
+//	parser.add(filenameArg);
+//
+//	parser.parse(argc, argv);
+//
+//	return {numberArg.getValue(), filenameArg.getValue()};
+//}
+
 ///
 /// Traite les arguments de ligne de commande à l'aide de TCLAP.
 ///
@@ -30,7 +79,7 @@ Arguments parseArgs(int argc, char* argv[]) {
 		"int"
 	);
 	parser.add(numberArg);
-	
+
 	// TODO: Créer l'argument de fichier (-f et --filename)
 	TCLAP::ValueArg<string> filenameArg(
 		"f",
@@ -44,6 +93,7 @@ Arguments parseArgs(int argc, char* argv[]) {
 
 	parser.parse(argc, argv);
 
-	return {numberArg.getValue(), filenameArg.getValue()};
+	return { numberArg.getValue(), filenameArg.getValue() };
 }
+
 
